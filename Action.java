4b725332db3c8df18eed2c39f840976ec353a3b7
Anidragon <81329162+Anidragon@users.noreply.github.com>
2023-03-25 17:15:08 -0400

    Update Action.java

--- Action.java
+++ Action.java
@@ -1,7 +1,8 @@
 
 public class Action {
-
+//this is the code for how an enemy performs attack actions all attacks performed by enemies are done through this class.
 	Enemies enemy;
+	
 	
 	public Action(Enemies e)
 	{
